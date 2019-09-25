4da47fa841866943fc948b42c4daf32da768188b
kirkh34 <kirkhoey@gmail.com>
2019-09-25 11:34:21 -0400

    Updates

---
+++ precache-manifest.5853e614977d1a43ad53051665ce298c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571be2d0a62dfb53ecab3e494cc7034b",
+    "url": "/Naxos/index.html"
+  },
+  {
+    "revision": "d3ae28dbdc7341265a42",
+    "url": "/Naxos/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "8aa7646002b15491b8d8",
+    "url": "/Naxos/static/css/main.ca9f02e5.chunk.css"
+  },
+  {
+    "revision": "d3ae28dbdc7341265a42",
+    "url": "/Naxos/static/js/2.1a1ae6bb.chunk.js"
+  },
+  {
+    "revision": "8aa7646002b15491b8d8",
+    "url": "/Naxos/static/js/main.f28b3798.chunk.js"
+  },
+  {
+    "revision": "c6220fa8cea331d0325b",
+    "url": "/Naxos/static/js/runtime-main.de58f4a8.js"
+  },
+  {
+    "revision": "77e4ae16960a50687d21cbf660a767e3",
+    "url": "/Naxos/static/media/alert.77e4ae16.mp3"
+  },
+  {
+    "revision": "09b60633d67e5f991bcd43e81e89a61f",
+    "url": "/Naxos/static/media/bg.09b60633.jpg"
+  }
+]);
