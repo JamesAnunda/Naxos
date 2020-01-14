4414c32851b0c5f6ae5aca57deebb91c20353c8c
JamesAnunda <cjanunda@gmail.com>
2020-01-14 09:34:20 -0500

    Update carrier.js
    Added render page and starting to make the login page

--- src/auth/carrier.js
+++ src/auth/carrier.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 //import { Link } from '@reach/router';
 import Firebase from './Firebase';
+import { render } from 'react-dom';
 
 
 class carrier extends Component {
 
 }
+
+render(); {
+    return (
+        <div>
+            <input>
+                name='email'
+            </input>
+        </div>
+    );
+};
